39f4f7bddd375077fc32d3b367648e383e0a3870
007 <007gzs@gmail.com>
2017-11-14 14:43:59 +0800

    add readme

--- src/main/java/com/github/binarywang/demo/wechat/WxOpenApplication.java
+++ src/main/java/com/github/binarywang/demo/wechat/WxOpenApplication.java
@@ -1,13 +1,11 @@
 package com.github.binarywang.demo.wechat;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author <a href="https://github.com/007gzs">007</a>
  */
 @SpringBootApplication
-@EnableAutoConfiguration
 public class WxOpenApplication {
     public static void main(String[] args) {
         SpringApplication.run(WxOpenApplication.class, args);
